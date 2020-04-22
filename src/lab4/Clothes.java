b92834211880146bf3af2cc39f21d85ba2d00bf3
Starling73 <ps.skvortsov@gmail.com>
2020-04-22 17:25:27 +0300

    all that I wanted to commit last time

---
+++ src/lab4/Clothes.java
@@ -0,0 +1,28 @@
+package lab4;
+
+public class Clothes {
+    private float price;
+    private int size;
+    private String type;
+    private String genter;
+    private String brand;
+
+    public Clothes(float price, int size, String type, String genter, String brand) {
+        this.price = price;
+        this.size = size;
+        this.type = type;
+        this.genter = genter;
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Clothes{" +
+                "price = " + price +
+                ", size = " + size +
+                ", type = '" + type + '\'' +
+                ", genter = '" + genter + '\'' +
+                ", brand = '" + brand + '\'' +
+                '}';
+    }
+}
